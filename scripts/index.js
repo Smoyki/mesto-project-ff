4d70b22e44a379a6b318f2ed2bf14472f7f2ff8c
Andrey Krotov <smoyki4456@yandex.ru>
2024-08-18 15:00:45 +0300

    dshgfbuyv

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,52 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template');
+
 // @todo: DOM узлы
 
+const places_list = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+function createCard (cardTemplate, element) { 
 
+    const cardElement = cardTemplate.content.querySelector('.places__item').cloneNode(true);  
+
+    cardElement.querySelector('.card__image').setAttribute('src', element.link); 
+
+    cardElement.querySelector('.card__image').setAttribute('alt', element.name); 
+
+    cardElement.querySelector('.card__title').textContent = element.name; 
+
+    
+
+    const deleteButton = cardElement.querySelector('.card__delete-button'); 
+
+
+
+    deleteButton.addEventListener('click', function(){ 
+
+        delCard(cardElement); 
+
+    }) 
+
+    return cardElement; 
+
+     
+
+} 
 // @todo: Функция удаления карточки
 
+
+function delCard (cardElement) { 
+
+    cardElement.remove(); 
+
+}
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(element => { 
+
+    const cardElement = createCard(cardTemplate, element); 
+
+    places_list.append(cardElement); 
+
+});
